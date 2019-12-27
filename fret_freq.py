2403115d5d5fdc2e1ba3d45f7bce314121b73836
Kevin Howlett <54913419+Kevin-Howlett@users.noreply.github.com>
2019-12-26 22:52:13 -0500

    Update fret_freq.py

--- fret_freq.py
+++ fret_freq.py
@@ -10,16 +10,6 @@ Created on Thu Dec 26 22:12:00 2019
 #(E standard tuning) and returns the resulting frequency
 
 def fret_freq(g_str, fret):
-    if g_str == 1:
-        string_frequency = 329.63
-    elif g_str == 2:
-        string_frequency = 246.94
-    elif g_str == 3:
-        string_frequency = 196
-    elif g_str == 4:
-        string_frequency = 146.83
-    elif g_str == 5:
-        string_frequency = 110
-    elif g_str == 6:
-        string_frequency = 82.41
-    return round(string_frequency * 2**(fret/12), 2)
+    f = {1: 329.63, 2: 246.94, 3: 196.00, 
+         4: 146.83, 5: 110.00, 6: 82.41}
+    return round(f[g_str] * (2**(fret/12)), 2)
